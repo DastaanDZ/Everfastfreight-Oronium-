f3264c63c1050801838d7ccdf05c83e5a0c5b164
Dastaan <vaibhav_b200642ce@nitc.ac.in>
2022-07-31 23:54:09 +0530

    added data to about us

--- component/aboutus/AboutUs.js
+++ component/aboutus/AboutUs.js
@@ -8,8 +8,8 @@ const index = () => {
       <div className={styles.container}>
         <div className={styles.left}>
         <div className={styles.aboutus}><p>ABOUT US</p></div>
-            <p className={styles.heading}>Ultricies amet risus in sit id risus nulla aliquet. </p>
-            <p className={styles.desc}>Amet magna pharetra lectus vivamus mattis. Sed id amet ipsum natoque sem diam. Enim sed eget enim ut imperdiet urna sit in enim. Ullamcorper tincidunt vulputate et in. Pharetra vel, massa porttitor vitae viverra ultrices velit. Nulla faucibus nunc egestas id. Id pellentesque odio fermentum sit massa sit amet vulputate risus.</p>
+            <p className={styles.heading}>Company Overview</p>
+            <p className={styles.desc}>Ever fast Freight Forwarders Pvt. Ltd., was founded in 1997 with motive to OFFER PEACE OF MIND, is an IATA recognized freight forwarder and are ISO 9001:2008 certified company by URS. We specialize in air, sea, export, and imports. We strongly believe that we are into communication business and transportation is part of it. Equipped with the array of services we are well prepared to accept any challenges evolved from current market trends and Endeavour to encourage your aspirations. We value time. The rules and regulations of international trade become more complex every day. It is vital that every exporter and importer sophisticated or novice, outsource these functions to experts who are experienced, informed and dedicated to the ultimate success of their business partnership.</p>
         </div>
         <div className={styles.right}>
             <img src={hero.src} className={styles.hero} alt="" />
--- component/cards/Card.js
+++ component/cards/Card.js
@@ -23,18 +23,44 @@ const index = () => {
     <div className={styles.card1}>
         <div className={styles.bullet}><p>1</p></div>
         <div className={styles.title}><p>Approach</p></div>
-        <div className={styles.description}><p>Interdum ultrices ante egestas gravida. Penatibus ornare tellus enim, non nulla. Commodo scelerisque nec vitae eget nulla nisi, magna et. Fusce.</p></div>
+        <div className={styles.description}>
+          <ul>
+            <li>Starts with knowing our customers.</li>
+            <li>Understanding their business.</li>
+            <li>Understanding our customer’s current and future requirements.</li>
+            <li>Designing our customer’s requirements and be responsive and efficient.</li>
+          </ul>
+        </div>
     </div>
     <div className={styles.card2}>
         <div className={styles.bullet}><p>2</p></div>
         <div className={styles.title}><p>Vision</p></div>
-        <div className={styles.description}><p>Turpis lobortis platea sed suspendisse nunc volutpat gravida velit. In ipsum malesuada morbi venenatis vel risus feugiat. Massa ipsum tincidunt iaculis ultricies quis massa facilisis proin.</p></div>
+        <div className={styles.description}>
+        <ul>
+            <li>To be the leading service provider, distinguished by the quality of our services. MISSION is to provide benefits to customers:</li>
+            <li>Providing outstanding operational quality in the industry.</li>
+            <li>Providing dedication to customers and their business.</li>
+            <li>Providing operational excellence and information.</li>
+            <li>Become a flexible, agile and innovative organization.</li>
+          </ul>
+        </div>
     </div>
     <div className={styles.card3} >
         <div className={styles.bullet}><p>3</p></div>
         <div className={styles.title}><p>Core Strength</p></div>
-        <div className={styles.description}><p>Turpis lobortis platea sed suspendisse nunc volutpat gravida velit. In ipsum malesuada morbi venenatis vel risus feugiat. Massa ipsum tincidunt iaculis ultricies quis massa facilisis proin.
-        Turpis lobortis platea sed suspendisse nunc volutpat gravida velit. In ipsum malesuada morbi venenatis vel risus feugiat. Massa ipsum tincidunt iaculis ultricies quis massa facilisis proin.</p></div>
+        <div className={styles.description}>
+        <ul>
+            <li>Cost Effective</li>
+            <li>Highly Sales Driven</li>
+            <li>Multi-location Presence</li>
+            <li>Global Operator with Local Knowledge</li>
+            <li>Young, Energetic and Dynamic Team</li>
+            <li>Consumer Focused Work Ethos</li>
+            <li>Multiple Product Range</li>
+            <li>Software Driven</li>
+            <li>Track and Trace Capability</li>
+          </ul>
+        </div>
     </div>
     </div>
     </>
