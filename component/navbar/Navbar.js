877425f04725c65c874c2640124300e6a7173b60
Dastaan <vaibhav_b200642ce@nitc.ac.in>
2022-08-07 22:48:02 +0530

    added drpodown in services

--- component/navbar/Navbar.js
+++ component/navbar/Navbar.js
@@ -2,18 +2,26 @@ import logo from "../../assets/images/logo.png";
 import styles from "./navbar.module.css";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { AiOutlineClose } from "react-icons/ai";
+import { BiDownArrow,BiUpArrow } from 'react-icons/bi';
 import { useState, useEffect } from "react";
 import Link from "next/link";
 
 const Navbar = () => {
-  const [toggle, setToggle] = useState(false);
 
-  const style = {
+  const [toggle, setToggle] = useState(false);
+  const [toggleLi, setToggleLi] = useState(false)
+
+  const style1 = {
     left: toggle ? "0" : "-100%",
     "@media (maxWidth: 768px)": {
       opacity: toggle ? "1" : "0",
     },
   };
+
+  const style2 = {
+      display: toggleLi? 'block' : 'none',
+  };
+  console.log(toggleLi)
 
   return (
     <>
@@ -23,33 +31,41 @@ const Navbar = () => {
             <img src={logo.src} className={styles.logo} alt="" />
           </div>
         </Link>
-        <div
-          className={styles.close}
-          onClick={() => {
+        <div className={styles.close}>
+          <GiHamburgerMenu onClick={() => {
             setToggle(!toggle);
-          }}
-        >
-          <GiHamburgerMenu />
+          }}/>
         </div>
         <ul
           className={styles.ul}
-          style={style}
-          onClick={() => {
-            setToggle(!toggle);
-          }}
+          style={style1}
         >
           <div className={styles.header}>
             <img src={logo.src} className={styles.logo} alt="" />
-            <AiOutlineClose />
+            <AiOutlineClose onClick={() => {
+            setToggle(!toggle);
+          }}/>
           </div>
-          <li className={styles.li}>Services</li>
+          <li className={styles.li}>
+          <p className={styles.services_li}>Services {toggleLi?<BiDownArrow className={styles.down_arrow} onClick={()=> setToggleLi(!toggleLi)}/>:<BiUpArrow className={styles.down_arrow} onClick={()=> setToggleLi(!toggleLi)}/>}</p>
+          <div className={styles.dropdown_services} style={style2}>
+            <ul>
+              <li>Air Freight</li>
+              <li>Sea Freight</li>
+              <li>Warehousing</li>
+              <li>Custom Clearence</li>
+              <li>Door to Door Services</li>
+              <li>Road Transport</li>
+              <li>Project Forwarding</li>
+            </ul>
+          </div>
+          </li>
           <Link href="/aboutus">
             <li className={styles.li}>About</li>
           </Link>
           <Link href="/branches">
             <li className={styles.li}>Branches</li>
           </Link>
-          {/* <li className={styles.li}>Track</li> */}
           <Link href="/career">
             <li className={styles.li}>Career</li>
           </Link>
